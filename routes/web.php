12397cb6c3dac5b96b621e23abf630d2f4bef1a4
Ademir Bastiani <ademir@phipasa.local>
2021-11-26 17:57:34 -0300

    one-to-one

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Preference;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,29 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/one-to-one', function () {
+    $user = User::with('preference')->find(2);
+
+    $data = ['background_color' => '#fff'];
+
+    if ($user->Preference) {
+        $user->preference->update($data);
+    } else {
+       // $user->preference()->create($data);
+       $preference = new Preference($data);
+       $user->preference()->save($preference);
+    }
+    
+    $user->refresh();
+
+    var_dump($user->preference);
+
+    $user->preference()->delete();
+    $user->refresh();
+
+    dd($user->preference);
+
+});
 
 Route::get('/', function () {
     return view('welcome');
